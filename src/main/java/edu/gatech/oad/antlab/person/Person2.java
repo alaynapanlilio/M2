39f1e450c7ef8a1e72e7d65f8bbaeb0138738e3f
Andrew Dallas <dadallas1202@gmail.com>
2018-09-06 22:37:14 -0400

    Completed calc() method in Person2.java

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -3,7 +3,7 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -31,14 +31,19 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+	  String output = "";
+	  for (int i = 1; i <= input.length(); i++) {
+	  	char temp = input.charAt((i + 1) % input.length());
+	  	output += temp;
+	  }
+	  return output;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
